9031816a80a9522b126ba128325856b8c3fdd966
Rodrigo Barrios <rodrigoedu11@gmail.com>
2025-08-22 16:24:48 -0500

    Basic listing of running containers

--- om-module/main.go
+++ om-module/main.go
@@ -1,7 +1,35 @@
 package main
+import (
+    "context"
+    "fmt"
+    "github.com/docker/docker/api/types/container"
+    "github.com/docker/docker/client"
+)
 
-import "fmt"
+func main() {
+    ctx := context.Background()
+    cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+    if err != nil {
+        panic(err)
+    }
+    defer cli.Close()
+    
+    containers, err := cli.ContainerList(ctx, container.ListOptions{})
+    if err != nil {
+        panic(err)
+    }
+    
+    for _, container := range containers {
+	name := "unnamed"
+   	if len(container.Names) > 0 {
+        	name = container.Names[0][1:] // Remove leading "/"
+    	}
 
-func main(){
-	fmt.Println("wenas")
+    	fmt.Printf("Name: %s\n", name)
+    	fmt.Printf("ID: %s\n", container.ID[:12])
+    	fmt.Printf("Image: %s\n", container.Image)
+    	fmt.Printf("Status: %s\n", container.Status)
+    	fmt.Printf("State: %s\n", container.State)
+    	fmt.Println("---")
+    }
 }
