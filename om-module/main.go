bd59c4bb3c2f211895cda54ba60fc19417496dff
Rodrigo Barrios <rodrigoedu11@gmail.com>
2025-08-22 18:05:00 -0500

    Small change

--- om-module/main.go
+++ om-module/main.go
@@ -1,35 +1,9 @@
 package main
+
 import (
-    "context"
-    "fmt"
-    "github.com/docker/docker/api/types/container"
-    "github.com/docker/docker/client"
+	discovery "github.com/Parz1val02/OM_module/discovery"
 )
 
 func main() {
-    ctx := context.Background()
-    cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-    if err != nil {
-        panic(err)
-    }
-    defer cli.Close()
-    
-    containers, err := cli.ContainerList(ctx, container.ListOptions{})
-    if err != nil {
-        panic(err)
-    }
-    
-    for _, container := range containers {
-	name := "unnamed"
-   	if len(container.Names) > 0 {
-        	name = container.Names[0][1:] // Remove leading "/"
-    	}
-
-    	fmt.Printf("Name: %s\n", name)
-    	fmt.Printf("ID: %s\n", container.ID[:12])
-    	fmt.Printf("Image: %s\n", container.Image)
-    	fmt.Printf("Status: %s\n", container.Status)
-    	fmt.Printf("State: %s\n", container.State)
-    	fmt.Println("---")
-    }
+	discovery.Discover()
 }
