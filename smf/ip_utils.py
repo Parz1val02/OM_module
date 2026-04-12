edbab9822bd2ed69947f1acfa536d4c1f80023ad
Rodrigo Barrios <rodrigoedu11@gmail.com>
2026-04-12 12:17:43 -0500

    Formatting

--- smf/ip_utils.py
+++ smf/ip_utils.py
@@ -24,9 +24,10 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import click
-import sys
 import ipaddress
+import sys
+
+import click
 
 """
 Script used to fetch first IP address in a given IP range. i.e. the calling bash script reads the std output
@@ -43,26 +44,28 @@ def validate_ip_net(ctx, param, value):
         ip_net = ipaddress.ip_network(value)
         return ip_net
     except ValueError:
-        raise click.BadParameter(
-            'Value does not represent a valid IPv4/IPv6 range')
+        raise click.BadParameter("Value does not represent a valid IPv4/IPv6 range")
 
 
 @click.command()
-@click.option('--ip_range',
-              required=True,
-              callback=validate_ip_net,
-              help='UE IPv4/IPv6 Address range in CIDR format e.g. 192.168.100.0/24 or 2001:230:cafe::/48')
+@click.option(
+    "--ip_range",
+    required=True,
+    callback=validate_ip_net,
+    help="UE IPv4/IPv6 Address range in CIDR format e.g. 192.168.100.0/24 or 2001:230:cafe::/48",
+)
 def start(ip_range):
 
     # Get the first IP address in the IP range and netmask prefix length
     first_ip_addr = next(ip_range.hosts(), None)
     if not first_ip_addr:
-        raise ValueError('Invalid UE IPv4 range. Only one IP given')
+        raise ValueError("Invalid UE IPv4 range. Only one IP given")
     else:
         first_ip_addr = first_ip_addr.exploded
         print(str(first_ip_addr))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     try:
         start()
         sys.exit(0)
--- upf/tun_if.py
+++ upf/tun_if.py
@@ -24,9 +24,10 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import click
-import subprocess
 import ipaddress
+import subprocess
+
+import click
 
 """
 Usage in command line:
@@ -40,44 +41,45 @@ def validate_ip_net(ctx, param, value):
         ip_net = ipaddress.ip_network(value)
         return ip_net
     except ValueError:
-        raise click.BadParameter(
-            'Value does not represent a valid IPv4/IPv6 range')
+        raise click.BadParameter("Value does not represent a valid IPv4/IPv6 range")
 
 
 @click.command()
-@click.option('--tun_ifname',
-              required=True,
-              help='TUN interface name e.g. ogstun')
-@click.option('--tun_ifmode',
-              required=True,
-              type=click.Choice(['tun', 'tap']),
-              help='TUN interface mode e.g. tun or tap')
-@click.option('--ipv4_range',
-              required=True,
-              callback=validate_ip_net,
-              help='UE IPv4 Address range in CIDR format e.g. 192.168.100.0/24')
-@click.option('--ipv6_range',
-              required=True,
-              callback=validate_ip_net,
-              help='UE IPv6 Address range in CIDR format e.g. 2001:230:cafe::/48')
-@click.option('--nat_rule',
-              default='yes',
-              help='Option specifying whether to add NATing iptables rule or not')
-def start(tun_ifname,
-          tun_ifmode,
-          ipv4_range,
-          ipv6_range,
-          nat_rule):
+@click.option("--tun_ifname", required=True, help="TUN interface name e.g. ogstun")
+@click.option(
+    "--tun_ifmode",
+    required=True,
+    type=click.Choice(["tun", "tap"]),
+    help="TUN interface mode e.g. tun or tap",
+)
+@click.option(
+    "--ipv4_range",
+    required=True,
+    callback=validate_ip_net,
+    help="UE IPv4 Address range in CIDR format e.g. 192.168.100.0/24",
+)
+@click.option(
+    "--ipv6_range",
+    required=True,
+    callback=validate_ip_net,
+    help="UE IPv6 Address range in CIDR format e.g. 2001:230:cafe::/48",
+)
+@click.option(
+    "--nat_rule",
+    default="yes",
+    help="Option specifying whether to add NATing iptables rule or not",
+)
+def start(tun_ifname, tun_ifmode, ipv4_range, ipv6_range, nat_rule):
 
     # Get the first IP address in the IP range and netmask prefix length
     first_ipv4_addr = next(ipv4_range.hosts(), None)
     if not first_ipv4_addr:
-        raise ValueError('Invalid UE IPv4 range. Only one IP given')
+        raise ValueError("Invalid UE IPv4 range. Only one IP given")
     else:
         first_ipv4_addr = first_ipv4_addr.exploded
     first_ipv6_addr = next(ipv6_range.hosts(), None)
     if not first_ipv6_addr:
-        raise ValueError('Invalid UE IPv6 range. Only one IP given')
+        raise ValueError("Invalid UE IPv6 range. Only one IP given")
     else:
         first_ipv6_addr = first_ipv6_addr.exploded
 
@@ -85,27 +87,71 @@ def start(tun_ifname,
     ipv6_netmask_prefix = ipv6_range.prefixlen
 
     # Setup the TUN/TAP interface, set IP address and setup IPtables
-    execute_bash_cmd('ip tuntap add name ' + tun_ifname + ' mode ' + tun_ifmode)
-    execute_bash_cmd('ip addr add ' + first_ipv4_addr + '/' +
-                     str(ipv4_netmask_prefix) + ' dev ' + tun_ifname)
-    execute_bash_cmd('ip addr add ' + first_ipv6_addr + '/' +
-                     str(ipv6_netmask_prefix) + ' dev ' + tun_ifname)
-    execute_bash_cmd('ip link set ' + tun_ifname + ' mtu 1450')
-    execute_bash_cmd('ip link set ' + tun_ifname + ' up')
-    if nat_rule == 'yes':
-        execute_bash_cmd('if ! iptables-save | grep -- \"-A POSTROUTING -s ' + ipv4_range.with_prefixlen + ' ! -o ' + tun_ifname + ' -j MASQUERADE\" ; then ' +
-                         'iptables -t nat -A POSTROUTING -s ' + ipv4_range.with_prefixlen + ' ! -o ' + tun_ifname + ' -j MASQUERADE; fi')
-        execute_bash_cmd('if ! ip6tables-save | grep -- \"-A POSTROUTING -s ' + ipv6_range.with_prefixlen + ' ! -o ' + tun_ifname + ' -j MASQUERADE\" ; then ' +
-                         'ip6tables -t nat -A POSTROUTING -s ' + ipv6_range.with_prefixlen + ' ! -o ' + tun_ifname + ' -j MASQUERADE; fi')
-        execute_bash_cmd('if ! iptables-save | grep -- \"-A INPUT -i ' + tun_ifname + ' -j ACCEPT\" ; then ' +
-                         'iptables -A INPUT -i ' + tun_ifname + ' -j ACCEPT; fi')
-        execute_bash_cmd('if ! ip6tables-save | grep -- \"-A INPUT -i ' + tun_ifname + ' -j ACCEPT\" ; then ' +
-                         'ip6tables -A INPUT -i ' + tun_ifname + ' -j ACCEPT; fi')
+    execute_bash_cmd("ip tuntap add name " + tun_ifname + " mode " + tun_ifmode)
+    execute_bash_cmd(
+        "ip addr add "
+        + first_ipv4_addr
+        + "/"
+        + str(ipv4_netmask_prefix)
+        + " dev "
+        + tun_ifname
+    )
+    execute_bash_cmd(
+        "ip addr add "
+        + first_ipv6_addr
+        + "/"
+        + str(ipv6_netmask_prefix)
+        + " dev "
+        + tun_ifname
+    )
+    execute_bash_cmd("ip link set " + tun_ifname + " mtu 1450")
+    execute_bash_cmd("ip link set " + tun_ifname + " up")
+    if nat_rule == "yes":
+        execute_bash_cmd(
+            'if ! iptables-save | grep -- "-A POSTROUTING -s '
+            + ipv4_range.with_prefixlen
+            + " ! -o "
+            + tun_ifname
+            + ' -j MASQUERADE" ; then '
+            + "iptables -t nat -A POSTROUTING -s "
+            + ipv4_range.with_prefixlen
+            + " ! -o "
+            + tun_ifname
+            + " -j MASQUERADE; fi"
+        )
+        execute_bash_cmd(
+            'if ! ip6tables-save | grep -- "-A POSTROUTING -s '
+            + ipv6_range.with_prefixlen
+            + " ! -o "
+            + tun_ifname
+            + ' -j MASQUERADE" ; then '
+            + "ip6tables -t nat -A POSTROUTING -s "
+            + ipv6_range.with_prefixlen
+            + " ! -o "
+            + tun_ifname
+            + " -j MASQUERADE; fi"
+        )
+        execute_bash_cmd(
+            'if ! iptables-save | grep -- "-A INPUT -i '
+            + tun_ifname
+            + ' -j ACCEPT" ; then '
+            + "iptables -A INPUT -i "
+            + tun_ifname
+            + " -j ACCEPT; fi"
+        )
+        execute_bash_cmd(
+            'if ! ip6tables-save | grep -- "-A INPUT -i '
+            + tun_ifname
+            + ' -j ACCEPT" ; then '
+            + "ip6tables -A INPUT -i "
+            + tun_ifname
+            + " -j ACCEPT; fi"
+        )
 
 
 def execute_bash_cmd(bash_cmd):
     return subprocess.run(bash_cmd, stdout=subprocess.PIPE, shell=True)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     start()
